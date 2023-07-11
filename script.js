15f2a4e00b8ef81acaafdbd4ca5aedd9194a9643
izinhah <133233107+izinhah@users.noreply.github.com>
2023-07-11 02:41:00 +0000

    Isa

--- script.js
+++ script.js
@@ -5,8 +5,33 @@ function contar() {
     let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) { 
-        window.alert('[ERRO] Faltam dados!') 
+        res.innerHTML = 'Imposível Contar!' 
+       // window.alert('[ERRO] Faltam dados!') 
+
     } else { 
-       res.innerHTML = Contando:
+       res.innerHTML = 'Contando:' 
+       let i = Number(ini.value) 
+       let f = Number(fim.value) 
+       let p = Number(passo.value) 
+       
+       if (p <= 0){ 
+        window.alert('Passo Inválido! Considerando passo 1') 
+        p = 1
+       } 
+
+       if (i < f){ 
+        //Contagem crescente
+            for(let c = i; c<= f; c += p) { 
+                res.innerHTML += ` ${c} \u{1F449}`
+            } 
+            
+       } else { 
+        //Contagem regressiva
+            for(let c = i; c >= f; c -= p) { 
+                resinnerHTML += `${c} \u{1F449}`
+            } 
+
+       }
+       res.innerHTML += `\u{1F3C1}`
     }
 }
