111439b947a4fb8dbc6633561ca0a912f9e596e4
izinhah <133233107+izinhah@users.noreply.github.com>
2023-07-11 23:58:39 +0000

    isabel

--- modelo2/script.js
+++ modelo2/script.js
@@ -1,3 +1,11 @@
 function Tabuada() { 
+    let num = document.getElementById('txtn') 
+    let tab = document.getElementById('txtseltab') 
+    if (num.value.length == 0) { 
+        let n = Number(num.value)  
+    } else { 
+        window.alert('Por favor digite um número!')
+    }
     
+
 }
