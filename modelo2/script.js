700cb96882971e419a15449c9741b28c7a897c9f
izinhah <133233107+izinhah@users.noreply.github.com>
2023-07-12 00:30:14 +0000

    isabela

--- modelo2/script.js
+++ modelo2/script.js
@@ -6,10 +6,12 @@ function Tabuada() {
   
     } else { 
         let n = Number(num.value) 
-        let c = 1; 
+        let c = 1 
+        tab.innerHTML = ``
         while (c <= 10) { 
             let item = document.createElement('option') 
-            item.text = `${num} x ${c} = ${num*c} ` 
+            item.text = `${n} x ${c} = ${n*c} ` 
+            item.value = tab${c}
             tab.appendChild(item) 
             c++
         }
