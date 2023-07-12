bd914747d01bc80dde64ec4775f49d17c88564dc
izinhah <133233107+izinhah@users.noreply.github.com>
2023-07-12 15:13:34 +0000

    isabel

--- modelo2/script.js
+++ modelo2/script.js
@@ -1,9 +1,8 @@
-function Tabuada() { 
+function tabuada() { 
     let num = document.getElementById('txtn') 
-    let tab = document.getElementById('txtseltab') 
+    let tab = document.getElementById('seltab') 
     if (num.value.length == 0) { 
         window.alert('Por favor digite um número!')
-  
     } else { 
         let n = Number(num.value) 
         let c = 1 
@@ -11,11 +10,10 @@ function Tabuada() {
         while (c <= 10) { 
             let item = document.createElement('option') 
             item.text = `${n} x ${c} = ${n*c} ` 
-            item.value = tab${c}
+            item.value = `tab${c}`
             tab.appendChild(item) 
             c++
         }
     }
     
-
 }
